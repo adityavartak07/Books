3f688fb4665eef081321960048bbe7cdcb1f5576
Aditya Vartak <adityavartak1001@gmail.com>
2021-05-24 17:47:52 +0530

    integrate

--- authenticate.js
+++ authenticate.js
@@ -6,17 +6,21 @@ var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var FacebookTokenStrategy = require('passport-facebook-token');
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
-
 var config = require('./config.js');
+const {OAuth2Client}=require('google-auth-library');
+
+const client=new OAuth2Client(config.google.clientId)
+
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken = function(user) {
-    return jwt.sign(user, config.secretKey,
-        {expiresIn: 3600});
-};
+// exports.getToken = function (user) {
+//      const token=jwt.sign(user, config.secretKey,
+//         { expiresIn: 3600 });
+//         return token
+// };
 
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
@@ -25,7 +29,7 @@ opts.secretOrKey = config.secretKey;
 exports.jwtPassport = passport.use(new JwtStrategy(opts,
     (jwt_payload, done) => {
         console.log("JWT payload: ", jwt_payload);
-        User.findOne({_id: jwt_payload._id}, (err, user) => {
+        User.findOne({ _id: jwt_payload._id }, (err, user) => {
             if (err) {
                 return done(err, false);
             }
@@ -38,9 +42,9 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
         });
     }));
 
-exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyUser = passport.authenticate('jwt', { session: false });
 
-exports.verifyAdmin = function(req, res, next) {
+exports.verifyAdmin = function (req, res, next) {
     if (req.user.admin) {
         next();
     }
@@ -56,32 +60,89 @@ exports.googlePassport = passport.use(new GoogleStrategy({
     clientID: config.google.clientId,
     clientSecret: config.google.clientSecret,
     callbackURL: "https://localhost:3443/users/auth/google/callback"
-  },
-  function(accessToken, refreshToken, email, done) {  User.findOne({
-    'googleId': email.id 
-}, function(err, user) {
-    console.log(email);
-    if (err) {
-        return done(err);
-    }
-    //No user was found... so create a new user with values from Facebook (all the profile. stuff)
-    if (!user) {
-        user = new User({
-            username: email.emails[0].value,           
-            googleId: email.id,
-            provider: 'google',
-            //now in the future searching on User.findOne({'facebook.id': profile.id } will match because of this next line
-           
+},
+    function (accessToken, refreshToken, email, done) {
+        User.findOne({
+            'googleId': email.id
+        }, function (err, user) {
+            console.log(email);
+            if (err) {
+                return done(err);
+            }
+            //No user was found... so create a new user with values from Facebook (all the profile. stuff)
+            if (!user) {
+                user = new User({
+                    email: email.emails[0].value,
+                    googleId: email.id,
+                    provider: 'google',
+                    //now in the future searching on User.findOne({'facebook.id': profile.id } will match because of this next line
+
+                });
+                user.save(function (err) {
+                    if (err) console.log(err);
+                    return done(err, user);
+                });
+            } else {
+                //found user. Return
+                return done(err, user);
+            }
         });
-        user.save(function(err) {
-            if (err) console.log(err);
-            return done(err, user);
-        });
-    } else {
-        //found user. Return
-        return done(err, user);
     }
-});
-}
 ))
-    
+
+// exports.googlelogin = (req, res) => {
+//     const { tokenId } = req.body;
+//     client.verifyIdToken({ idToken: tokenId, audience: config.google.clientId }).then((res) => {
+//         const { email_verified, name, email } = res.payload;
+//         console.log(res.payload, "data")
+//         console.log(tokenId)
+//         if (email_verified) {
+//             User.findOne({ email: res.payload.email }).exec((err, user) => {
+//                 if (err) {
+                    
+//                     res.statusCode = 400;
+//                     res.json={
+//                         error: "Something went wrong"
+//                     }
+                     
+//                 } else {
+//                     if (user) {
+//                         const token=jwt.sign({user}, config.secretKey,
+//                             { expiresIn: 3600 });
+//                         res.json={
+//                             token,
+//                             user:{user}
+//                         }
+                            
+//                     } else {
+//                         user = new User({
+//                             email: res.payload.email,
+//                             provider: 'google',
+//                             //now in the future searching on User.findOne({'facebook.id': profile.id } will match because of this next line
+
+//                         });
+//                         user.save((err,user) => {
+//                             if (err) 
+//                             {
+//                                 res.statusCode = 400;
+//                                 res.json={
+//                                     error: "Something went wrong"
+//                                 }
+//                             }
+//                             const token=jwt.sign({user}, config.secretKey,
+//                                 { expiresIn: 3600 });
+//                             res.json={
+//                             token,
+//                             user:{user}
+//                         }
+                            
+//                         });
+
+//                     }
+//                 }
+
+//             })
+
+//         }
+//     })
+// }
--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb+srv://user1:qwerty123@cluster0.hpldh.mongodb.net/abcd?retryWrites=true&w=majority',
+    'mongoUrl':'mongodb://localhost:27017/confusion',
+    // 'mongoUrl' : 'mongodb+srv://user1:qwerty123@cluster0.hpldh.mongodb.net/abcd?retryWrites=true&w=majority',
     'google': {
-        clientId: '405841535069-5ivvh3rvi9mt8f1g9ne5fg97vftlfl5j.apps.googleusercontent.com',
-        clientSecret: 'FPCe5PwBQPfOEVremU3znF5c'
+        clientId:"407417922894-som50la42tob4gdptrs7v05m6s6cq71r.apps.googleusercontent.com",
+        clientSecret:'v6LpXeueCaVPnKFYzqg8K4ia'
     }
 }
